df4aa69fc508bb64530126a92fb0b00090741a49
konstantinnovation <konstans@stuy.edu>
2022-09-30 12:11:54 -0400

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,15 @@
+public class StringMethods{
+
+  //return the index of s that contains the first occurence of target, 
+  //or -1 if it is not present. Obviously do not use indexOf().
+  public static int myIndexOf(String s, String target){
+    return 0;
+  }
+  
+  //return 0 if the strings are equal
+  //return a positive number if other is less than s
+  //return a negative number if other is greater than s
+  public static int myCompareTo(String s, String other){
+    return 0;
+  }
+}
