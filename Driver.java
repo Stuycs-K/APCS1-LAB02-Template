5a8e38fd4792507997e93f844c60bbe4f0f3fe23
konstantinnovation <konstans@stuy.edu>
2023-09-28 08:52:08 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,9 @@
 public class Driver{
  public static void main(String[]args){
+   System.out.println("Expected 0, my function: "+StringMethods.myCompareTo("Hello","Hello")); 
+   System.out.println("Expected a negative number, my function: "+StringMethods.myCompareTo("A","B")); 
+   System.out.println("Expected -1, my function: "+Stringmethods.myIndexOf("Hello","he"));  
+   System.out.println("Expected 1, my function: "+Stringmethods.myIndexOf("Hello","el")); 
    
  }
 }
