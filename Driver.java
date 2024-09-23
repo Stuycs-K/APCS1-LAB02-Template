b2cb81d9892a3d1af2d57ed52742dd5335e7b47d
konstantinnovation <konstans@stuy.edu>
2024-09-23 08:53:07 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,25 +4,24 @@ public class Driver{
   String b;
   String whole;
   String part;
-  
+  String number;
   //Can you think of why the test cases use variables instead of literal strings?
   
   //Single test case myCompareTo:
   a="cat";
   b="dog";
   System.out.println("Expected "+ a.CompareTo(b) +" vs my function: "+StringMethods.myCompareTo(a,b)); 
-   
+
+  
   //Single test case myIndexOf:
   whole = "Hello";
   part = "He";
   System.out.println("Expected "+ whole.indexOf(part) +" vs my function: "+StringMethods.myIndexOf(whole,part));  
-   
-  
-  String number;
+    
   //Single test case parseInt:
-  // number = "152";
-  // int actual = Integer.parseInt(number);
-  // int guess  = StringMethods.parseInt(number);
-  // System.out.println("Expected "+ actual +" vs my function: "+ guess+" "+(actual==guess));
+  number = "152";
+  int actual = Integer.parseInt(number);
+  int guess  = StringMethods.parseInt(number);
+  System.out.println("Expected "+ actual +" vs my function: "+ guess+" "+(actual==guess));
  }
 }
