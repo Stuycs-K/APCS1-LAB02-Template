4415eeaabfa58adc1fbf9f2c05d7d97598e36a1b
konstantinnovation <konstans@stuy.edu>
2024-09-23 08:52:25 -0400

    Update StringMethods.java

--- StringMethods.java
+++ StringMethods.java
@@ -1,7 +1,8 @@
 public class StringMethods{
 
   //return the index of s that contains the first occurence of target, 
-  //or -1 if it is not present. Obviously do not use indexOf().
+  //or -1 if it is not present. 
+  //Do not use indexOf().
   public static int myIndexOf(String s, String target){
     return 0;
   }
@@ -9,7 +10,15 @@ public class StringMethods{
   //return 0 if the strings are equal
   //return a positive number if other is less than s
   //return a negative number if other is greater than s
+  //Do not use compareTo().
   public static int myCompareTo(String s, String other){
+    return 0;
+  }
+
+  //return the integer value of the String.
+  //precondition: s is formatted as a valid integer.
+  //Do not use Integer.parseInt()
+  public static int parseInt(String s){
     return 0;
   }
 }
